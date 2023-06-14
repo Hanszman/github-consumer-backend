95d44aca7d5b030fd39aaa7f4e903a4b777f6b5e
Hanszman <victor.hanszman@hotmail.com>
2023-06-14 20:42:29 -0300

    comments

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ const user = require('./controllers/userController.js');
 routes.get('/', (req, res) => res.json({ msg: 'API Online!' }));
 routes.get('/api', (req, res) => res.json({ msg: 'Select the right path!' }));
 routes.get('/api/users', user.userList); // This endpoint returns a list of GitHub users and the link for the next page.
-routes.get('/api/users/:username/details', user.userDetails); // This endpoint returns the details of a GitHub user
-routes.get('/api/users/:username/repos', user.userRepos); // This endpoint returns a list with all user repositories
+routes.get('/api/users/:username/details', user.userDetails); // This endpoint returns the details of a GitHub user.
+routes.get('/api/users/:username/repos', user.userRepos); // This endpoint returns a list with all user repositories.
 
 // Exportation
 module.exports = routes;
