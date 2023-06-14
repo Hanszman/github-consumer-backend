ed033a2e29183d28e7718aba23bc36d803a1d231
Hanszman <victor.hanszman@hotmail.com>
2023-06-14 20:41:04 -0300

    users endpoints link to the next page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,22 +3,24 @@ const gitAPI = require('../services/gitApi.js');
 
 // Controller Functions
 const userList = async (req, res) => {
-    let params = req.query;
-    let since = params.since;
-    let perPage = params.per_page;
-    let result = await gitAPI.octokit.request("GET /users?since={since}&per_page={perPage}", {since, perPage});
+    const params = req.query;
+    const since = params.since ? params.since : '';
+    const perPage = params.per_page ? params.per_page : '';
+    const result = await gitAPI.octokit.request("GET /users?since={since}&per_page={perPage}", {since, perPage});
+    const lastUser = result.data[result.data.length-1].id;
+    result.nextPageUrl = `${process.env.HOST}:${process.env.PORT}/api/users?since=${lastUser}&per_page=${perPage}`;
     res.status(result.status).json({response: result});
 }
 
 const userDetails = async (req, res) => {
-    let user = req.params.username;
-    let result = await gitAPI.octokit.request("GET /users/{user}", {user});
+    const user = req.params.username;
+    const result = await gitAPI.octokit.request("GET /users/{user}", {user});
     res.status(result.status).json({response: result});
 }
 
 const userRepos = async (req, res) => {
-    let user = req.params.username;
-    let result = await gitAPI.octokit.request("GET /users/{user}/repos", {user});
+    const user = req.params.username;
+    const result = await gitAPI.octokit.request("GET /users/{user}/repos", {user});
     res.status(result.status).json({response: result});
 }
 
