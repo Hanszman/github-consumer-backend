11134a45639a122c3b79be9da374f41b4e3d191a
Hanszman <victor.hanszman@hotmail.com>
2023-06-15 00:53:58 -0300

    try catch fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,25 +3,46 @@ const gitAPI = require('../services/gitApi.js');
 
 // Controller Functions
 const userList = async (req, res) => {
-    const params = req.query;
-    const since = params.since ? params.since : '';
-    const perPage = params.per_page ? params.per_page : '';
-    const result = await gitAPI.octokit.request("GET /users?since={since}&per_page={perPage}", {since, perPage});
-    const lastUser = result.data[result.data.length-1].id;
-    result.nextPageUrl = `${process.env.HOST}:${process.env.PORT}/api/users?since=${lastUser}&per_page=${perPage}`;
-    res.status(result.status).json({response: result});
+    try {
+        const params = req.query;
+        const since = params.since ? params.since : '';
+        const perPage = params.per_page ? params.per_page : '';
+        const result = await gitAPI.octokit.request("GET /users?since={since}&per_page={perPage}", {since, perPage});
+        if (result?.status === 200) {
+            const lastUser = result.data[result.data.length-1].id;
+            result.nextPageUrl = `${process.env.HOST}:${process.env.PORT}/api/users?since=${lastUser}&per_page=${perPage}`;
+            res.status(result.status).json({error: false, response: result});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(error.status).json({error: true, response: {}});
+    }
 }
 
 const userDetails = async (req, res) => {
-    const user = req.params.username;
-    const result = await gitAPI.octokit.request("GET /users/{user}", {user});
-    res.status(result.status).json({response: result});
+    try {
+        const user = req.params.username;
+        const result = await gitAPI.octokit.request("GET /users/{user}", {user});
+        if (result?.status === 200) {
+            res.status(result.status).json({error: false, response: result});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(error.status).json({error: true, response: {}});
+    }
 }
 
 const userRepos = async (req, res) => {
-    const user = req.params.username;
-    const result = await gitAPI.octokit.request("GET /users/{user}/repos", {user});
-    res.status(result.status).json({response: result});
+    try {
+        const user = req.params.username;
+        const result = await gitAPI.octokit.request("GET /users/{user}/repos", {user});
+        if (result?.status === 200) {
+            res.status(result.status).json({error: false, response: result});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(error.status).json({error: true, response: {}});
+    }
 }
 
 // Exportation
